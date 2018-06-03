78756cabbbadcbd1ec3cc13afbb0a3c5a91e5179
Roger Vaca <rogervaca@gmail.com>
2018-06-03 17:49:32 -0400

    Render invalid days at start

--- script.js
+++ script.js
@@ -25,6 +25,55 @@ $('document').ready(function() {
     console.log("Final Month: " + (finalMonth + 1));
     var totalMonths = (finalMonth + (years * 12)) - initialMonth + 1;
     console.log("Total Months: " + totalMonths);
-    
+    var month = initialMonth;
+    for (m = 0; m < totalMonths; m++){      
+      var monthStartsAt = new Date(initialDate.getFullYear(), month, 1);
+      var firstValidDayOfMonth;
+      if (initialDate < monthStartsAt){
+        firstValidDayOfMonth = monthStartsAt;
+      }
+      else{
+        firstValidDayOfMonth = initialDate;
+      }
+      
+      /*add initials of the days as header*/
+      var tableHeader = '<table class="table-bordered">';
+      tableHeader += '<thead><tr><th>S</th><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th></tr>';
+      var html = tableHeader;
+
+      /*get the month and the year of the given date and add it to the header*/
+      var monthLabel = monthNames[month];
+      console.log('Month: ' + monthLabel);
+      var monthLabel = monthLabel + " - " + initialDate.getFullYear();
+      html += '<tr><th class="text-center" colspan="7">' + monthLabel + '</th></tr></thead>';
+
+      /*start filling invalid days at the beginning of the calendar*/
+      var initialDayOfWeek = monthStartsAt.getDay();
+      var initialDay = firstValidDayOfMonth.getDate();
+      var initialInvalidDays = initialDay + initialDayOfWeek - 1;
+
+      var dayOfWeek = 0;
+      html += "<tbody>";
+      console.log("Invalid days: " + initialInvalidDays);
+      for (i = 0; i < initialInvalidDays; i++) {
+        if (dayOfWeek == 0){
+          html += '<tr>';
+        }
+        html += '<td>&nbsp;</td>';
+        dayOfWeek++;
+        if(dayOfWeek == 7)
+        {
+          html += '</tr>';
+          dayOfWeek = 0;
+        }
+      }
+
+      html += '</tbody></table>';
+      console.log(html);
+      $('#calendar').append(html);
+
+      /*set next month value*/
+      month++;
+    }
   });
 });
